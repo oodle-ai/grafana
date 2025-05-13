6f80ef8ea813cabea5f572cff30e9557378802fb
Vijay Karthik <vijay.karthik@oodle.ai>
2025-05-13 12:15:22 -0700

    debug

--- public/app/plugins/panel/oodlelogs/TextPanel.tsx
+++ public/app/plugins/panel/oodlelogs/TextPanel.tsx
@@ -9,6 +9,53 @@ import { defaultOptions, Options } from './panelcfg.gen';
 
 export interface Props extends PanelProps<Options> { }
 
+const openSearchCSSMods = `
+#queryLanguageSwitcherPopover,
+.euiListGroupItem__label[title="Overview"],
+a[data-test-subj="docTableRowAction-0"],
+a[data-test-subj="docTableRowAction-1"],
+button[aria-label="Toggle primary navigation"],
+div[data-test-subj="collapsibleNavGroup-recentlyViewed"],
+div[data-test-subj="collapsibleNavGroup-observability"],
+div[data-test-subj="collapsibleNavGroup-opensearch"],
+div[data-test-subj="collapsibleNavGroup-management"] {
+  display: none !important;
+}
+
+/* Hide the "home" icon on the top left */
+a.header__homeLoaderNavButton {
+  width: 0px !important;
+  min-width: 0px !important;
+  border: 0px !important;
+}
+
+/* The loading spinner used to show in place of the "home" icon.
+   Move it to the top right corner instead. */
+span[data-test-subj="globalLoadingIndicator"] {
+  position: absolute;
+  left: calc(100vw - 36px);
+  top: 12px;
+  width: 24px;
+  height: 24px;
+}
+
+.euiBody--headerIsFixed {
+  padding-top: 0px !important;
+}
+
+.primaryHeader {
+    display: none;
+    height: 0px !important;
+}
+
+.osdOverviewPageHeader__actionItem:last-child {
+  display: none;
+}
+
+.deLayout {
+  height: 100% !important;
+}
+`
 export function TextPanel(props: Props) {
   const styles = useStyles2(getStyles);
   const iframeRef = useRef<HTMLIFrameElement>(null);
@@ -61,6 +108,8 @@ export function TextPanel(props: Props) {
   const injectLocationCallback = () => {
     const iframe = iframeRef.current;
     if (!iframe) { return; }
+
+    injectCSS(iframe, openSearchCSSMods);
 
     if (mutationObserverRef.current) {
       mutationObserverRef.current.disconnect();
@@ -127,9 +176,9 @@ const getStyles = (theme: GrafanaTheme2) => ({
     }
   `,
   markdown: cx(
-    'markdown-html',
     css`
       height: 100%;
+      border: none !important;
     `
   ),
   containStrict: css({
@@ -159,3 +208,26 @@ function setupLocationChangeCallback(
   observer.observe(doc, { subtree: true, childList: true });
   return observer;
 }
+
+export function injectCSS(iframe: HTMLIFrameElement, css?: string) {
+  // UUID to identify the injected element, to avoid any conflicts with
+  // actual ID any HTML element might have.
+  const styleID = '277f78d1-06cc-4b3e-9c5a-5c537ad976ac';
+
+  if (!css) {return;}
+
+  const doc = iframe.contentDocument;
+  if (!doc) {return;}
+
+  let oldCSS = doc.getElementById(styleID);
+  if (oldCSS) {
+    if (oldCSS.textContent === css) {return;}
+    oldCSS.remove();
+  }
+
+  const style = doc.createElement('style');
+  style.id = styleID;
+  style.appendChild(doc.createTextNode(css));
+  doc.head?.appendChild(style);
+}
+
