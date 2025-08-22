ce044f9c403e89f469a23600196f57c8ae605dc5
Anirud Samala <anirud.samala@oodle.ai>
2025-08-22 14:16:22 -0700

    discuss-1

--- public/app/features/dashboard-scene/saving/getDashboardChangesFromScene.ts
+++ public/app/features/dashboard-scene/saving/getDashboardChangesFromScene.ts
@@ -1,7 +1,35 @@
+import { Panel } from '@grafana/schema';
 import { DashboardScene } from '../scene/DashboardScene';
 import { transformSceneToSaveModel } from '../serialization/transformSceneToSaveModel';
 
 import { getDashboardChanges as getDashboardSaveModelChanges } from './getDashboardChanges';
+import { sortedDeepCloneWithoutNulls } from 'app/core/utils/object';
+
+export function getSortedPanels(panels: Panel[]): Panel[] {
+  const rowIdxs = panels.reduce((acc, panel, idx) => {
+    if (panel.type === 'row') {
+      return [...acc, idx];
+    } else {
+      return acc;
+    }
+  }, [0]).concat(panels.length-1);
+
+  let sortedPanels: Panel[] = [];
+  let tempPanels: Panel[] = [];
+
+  for(let i = 0; i < panels.length; i++) {
+    if(rowIdxs.includes(i)) {
+      sortedPanels.push(...tempPanels.sort((a, b) => (a?.id ?? 0) - (b?.id ?? 0)));
+      tempPanels = [];
+    } else {
+      tempPanels.push(panels[i]);
+    }
+  }
+
+  sortedPanels.push(...tempPanels.sort((a, b) => (a?.id ?? 0) - (b?.id ?? 0)));
+  
+  return sortedPanels;
+}
 
 /**
  * Get changes between the initial save model and the current scene.
@@ -18,9 +46,11 @@ export function getDashboardChangesFromScene(
   saveVariables?: boolean,
   saveRefresh?: boolean
 ) {
+  const initialSaveModel = scene.getInitialSaveModel()!;
+  const currentSceneModel = transformSceneToSaveModel(scene);
   const changeInfo = getDashboardSaveModelChanges(
-    scene.getInitialSaveModel()!,
-    transformSceneToSaveModel(scene),
+    sortedDeepCloneWithoutNulls({...initialSaveModel, panels: getSortedPanels(initialSaveModel.panels ?? [])}),
+    sortedDeepCloneWithoutNulls({...currentSceneModel, panels: getSortedPanels(currentSceneModel.panels ?? [])}),
     saveTimeRange,
     saveVariables,
     saveRefresh
