23a2a891618abe8da1cc4b2f8efefaa202daa143
Anirud Samala <anirud.samala@oodle.ai>
2025-09-11 01:58:24 +0530

    no profiling flag

--- public/swagger/index.tsx
+++ public/swagger/index.tsx
@@ -15,7 +15,6 @@ if (window.nonce) {
   __webpack_nonce__ = window.nonce;
 }
 
-// Import statements should be at the top of the file
 import 'swagger-ui-react/swagger-ui.css';
 
 import DOMPurify from 'dompurify';
