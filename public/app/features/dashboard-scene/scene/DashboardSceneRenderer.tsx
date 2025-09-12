ad6b6e2270f575d01c543e02ee0abdf7fa0f7c08
Anirud Samala <anirud.samala@oodle.ai>
2025-09-12 20:00:08 +0530

    Refactor DashboardSceneRenderer

--- public/app/features/dashboard-scene/scene/DashboardSceneRenderer.tsx
+++ public/app/features/dashboard-scene/scene/DashboardSceneRenderer.tsx
@@ -1,5 +1,5 @@
 import { css, cx } from '@emotion/css';
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom-v5-compat';
 
 import { GrafanaTheme2, PageLayoutType } from '@grafana/data';
@@ -18,22 +18,76 @@ import { NavToolbarActions, ToolbarActions } from './NavToolbarActions';
 import { PanelSearchLayout } from './PanelSearchLayout';
 import { DashboardAngularDeprecationBanner } from './angular/DashboardAngularDeprecationBanner';
 
-export function DashboardSceneRenderer({ model }: SceneComponentProps<DashboardScene>) {
-  const { controls, overlay, editview, editPanel, isEmpty, meta, viewPanelScene, panelSearch, panelsPerRow } =
+function DashboardSceneRendererBody({ model }: SceneComponentProps<DashboardScene>) {
+  const { controls, isEmpty, meta, panelSearch, panelsPerRow } =
     model.useState();
   const headerHeight = useChromeHeaderHeight();
   const styles = useStyles2(getStyles, headerHeight ?? 0);
+  const bodyToRender = model.getBodyToRender();
+  const hasControls = controls?.hasControls();
+
+  if (meta.dashboardNotFound) {
+    return <EntityNotFound entity="Dashboard" key="dashboard-not-found" />;
+  }
+
+  if (isEmpty) {
+    return (
+      <>
+        <DashboardEmpty dashboard={model} canCreate={!!model.state.meta.canEdit} key="dashboard-empty-state" />
+        <div className={cx(styles.body, !hasControls && styles.bodyWithoutControls)} key="dashboard-panels">
+          <bodyToRender.Component model={bodyToRender} />
+        </div>
+      </>
+    );
+  }
+
+  if (panelSearch || panelsPerRow) {
+    return <PanelSearchLayout panelSearch={panelSearch} panelsPerRow={panelsPerRow} dashboard={model} />;
+  }
+
+  return (
+    <>
+      <DashboardAngularDeprecationBanner dashboard={model} key="angular-deprecation-banner" />
+      <div className={cx(styles.body, !hasControls && styles.bodyWithoutControls)} key="dashboard-panels">
+        <bodyToRender.Component model={bodyToRender} />
+      </div>
+    </>
+  );
+}
+
+function DashboardSceneRendererContent({ model }: SceneComponentProps<DashboardScene>) {
+  const { controls } = model.useState();
+  const headerHeight = useChromeHeaderHeight();
+  const styles = useStyles2(getStyles, headerHeight ?? 0);
+  const hasControls = controls?.hasControls();
+  const isSingleTopNav = config.featureToggles.singleTopNav;
+
+  return (
+    <div className={cx(styles.pageContainer, hasControls && styles.pageContainerWithControls)}>
+      {!isSingleTopNav && <NavToolbarActions dashboard={model} />}
+      {controls && (
+        <div className={styles.controlsWrapper}>
+          <controls.Component model={controls} />
+        </div>
+      )}
+      <div className={cx(styles.canvasContent)}>
+        <DashboardSceneRendererBody model={model} />
+      </div>
+    </div>
+  );
+}
+
+export function DashboardSceneRenderer({ model }: SceneComponentProps<DashboardScene>) {
+  const { overlay, editview, editPanel, viewPanelScene } = model.useState();
   const location = useLocation();
   const navIndex = useSelector((state) => state.navIndex);
   const pageNav = model.getPageNav(location, navIndex);
-  const bodyToRender = model.getBodyToRender();
   const navModel = getNavModel(navIndex, 'dashboards/browse');
-  const hasControls = controls?.hasControls();
   const isSettingsOpen = editview !== undefined;
   const isSingleTopNav = config.featureToggles.singleTopNav;
 
   // Remember scroll pos when going into view panel, edit panel or settings
-  useMemo(() => {
+  useEffect(() => {
     if (viewPanelScene || isSettingsOpen || editPanel) {
       model.rememberScrollPos();
     }
@@ -55,30 +109,6 @@ export function DashboardSceneRenderer({ model }: SceneComponentProps<DashboardS
     );
   }
 
-  const emptyState = (
-    <DashboardEmpty dashboard={model} canCreate={!!model.state.meta.canEdit} key="dashboard-empty-state" />
-  );
-
-  const withPanels = (
-    <div className={cx(styles.body, !hasControls && styles.bodyWithoutControls)} key="dashboard-panels">
-      <bodyToRender.Component model={bodyToRender} />
-    </div>
-  );
-
-  const notFound = meta.dashboardNotFound && <EntityNotFound entity="Dashboard" key="dashboard-not-found" />;
-
-  const angularBanner = <DashboardAngularDeprecationBanner dashboard={model} key="angular-deprecation-banner" />;
-
-  let body: React.ReactNode = [angularBanner, withPanels];
-
-  if (notFound) {
-    body = [notFound];
-  } else if (isEmpty) {
-    body = [emptyState, withPanels];
-  } else if (panelSearch || panelsPerRow) {
-    body = <PanelSearchLayout panelSearch={panelSearch} panelsPerRow={panelsPerRow} dashboard={model} />;
-  }
-
   return (
     <Page
       navId={'dashboard-scene-renderer'}
@@ -89,15 +119,7 @@ export function DashboardSceneRenderer({ model }: SceneComponentProps<DashboardS
     >
       {editPanel && <editPanel.Component model={editPanel} />}
       {!editPanel && (
-        <div className={cx(styles.pageContainer, hasControls && styles.pageContainerWithControls)}>
-          {!isSingleTopNav && <NavToolbarActions dashboard={model} />}
-          {controls && (
-            <div className={styles.controlsWrapper}>
-              <controls.Component model={controls} />
-            </div>
-          )}
-          <div className={cx(styles.canvasContent)}>{body}</div>
-        </div>
+        <DashboardSceneRendererContent model={model} />
       )}
       {overlay && <overlay.Component model={overlay} />}
     </Page>
@@ -107,6 +129,7 @@ export function DashboardSceneRenderer({ model }: SceneComponentProps<DashboardS
 function getStyles(theme: GrafanaTheme2, headerHeight: number) {
   return {
     pageContainer: css({
+      label: 'page-container-dashboard-scene-renderer',
       display: 'grid',
       gridTemplateAreas: `
   "panels"`,
