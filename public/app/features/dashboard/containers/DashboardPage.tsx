b1ea1d3418f93baca69cce96b8082fd07b443819
Anirud Samala <anirud.samala@oodle.ai>
2025-09-12 13:43:11 +0530

    Clean up setTimeouts

--- public/app/features/dashboard/containers/DashboardPage.tsx
+++ public/app/features/dashboard/containers/DashboardPage.tsx
@@ -125,6 +125,7 @@ export class UnthemedDashboardPage extends PureComponent<Props, State> {
   static contextType = GrafanaContext;
 
   private forceRouteReloadCounter = 0;
+  private liveTimerTimeout?: NodeJS.Timeout;
   state: State = this.getCleanState();
 
   getCleanState(): State {
@@ -144,6 +145,10 @@ export class UnthemedDashboardPage extends PureComponent<Props, State> {
   }
 
   componentWillUnmount() {
+    // Clear any pending live timer timeout to prevent memory leaks
+    clearTimeout(this.liveTimerTimeout);
+    this.liveTimerTimeout = undefined;
+    
     this.closeDashboard();
   }
 
@@ -172,7 +177,7 @@ export class UnthemedDashboardPage extends PureComponent<Props, State> {
     });
 
     // small delay to start live updates
-    setTimeout(this.updateLiveTimer, 250);
+    this.liveTimerTimeout = setTimeout(this.updateLiveTimer, 250);
   }
 
   componentDidUpdate(prevProps: Props, prevState: State) {
