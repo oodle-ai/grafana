7bcb935c0c003fdfd274ddb9174f585af22b9a35
Vijay Karthik <vijay.karthik@oodle.ai>
2025-11-14 17:57:35 -0800

    Update border radius

--- packages/grafana-data/src/themes/createShape.ts
+++ packages/grafana-data/src/themes/createShape.ts
@@ -22,7 +22,7 @@ export function createShape(options: ThemeShapeInput): ThemeShape {
   const baseBorderRadius = options.borderRadius ?? 2;
 
   const radius = {
-    default: '0.375rem',
+    default: '0.125rem',
     pill: '9999px',
     circle: '100%',
   };
