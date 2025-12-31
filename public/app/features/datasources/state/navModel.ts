36814bd7886a35de945406475acbf114d7671c10
Vijay Karthik <vijay.karthik@oodle.ai>
2025-12-31 15:46:42 -0800

    Remove unnecessary tabs in datasource page

--- public/app/features/datasources/state/navModel.ts
+++ public/app/features/datasources/state/navModel.ts
@@ -1,12 +1,6 @@
 import { DataSourceSettings, PluginType, PluginInclude, NavModel, NavModelItem } from '@grafana/data';
 import { t } from '@grafana/i18n';
-import { featureEnabled } from '@grafana/runtime';
-import { ProBadge } from 'app/core/components/Upgrade/ProBadge';
-import config from 'app/core/config';
 import { contextSrv } from 'app/core/core';
-import { isOpenSourceBuildOrUnlicenced } from 'app/features/admin/EnterpriseAuthFeaturesCard';
-import { highlightTrial } from 'app/features/admin/utils';
-import { AccessControlAction } from 'app/types/accessControl';
 import icnDatasourceSvg from 'img/icn-datasource.svg';
 
 import { GenericDataSourcePlugin } from '../types';
@@ -15,7 +9,6 @@ const loadingDSType = 'Loading';
 
 export function buildNavModel(dataSource: DataSourceSettings, plugin: GenericDataSourcePlugin): NavModelItem {
   const pluginMeta = plugin.meta;
-  const highlightsEnabled = config.featureToggles.featureHighlights;
   const navModel: NavModelItem = {
     img: pluginMeta.info.logos.large,
     id: 'datasource-' + dataSource.uid,
@@ -54,96 +47,15 @@ export function buildNavModel(dataSource: DataSourceSettings, plugin: GenericDat
     });
   }
 
-  const shouldEnableFeatureHighlights = isOpenSourceBuildOrUnlicenced();
-
-  const isLoadingNav = dataSource.type === loadingDSType;
-
-  const permissionsExperimentId = 'feature-highlights-data-source-permissions-badge';
-  const dsPermissions: NavModelItem = {
-    active: false,
-    icon: 'lock',
-    id: `datasource-permissions-${dataSource.uid}`,
-    text: t('datasources.build-nav-model.ds-permissions.text.permissions', 'Permissions'),
-    url: `datasources/edit/${dataSource.uid}/permissions`,
-  };
-
-  if ((highlightTrial() && !isLoadingNav) || shouldEnableFeatureHighlights) {
-    dsPermissions.tabSuffix = () => ProBadge({ experimentId: permissionsExperimentId, eventVariant: 'trial' });
-  }
-
-  if (featureEnabled('dspermissions.enforcement') || shouldEnableFeatureHighlights) {
-    if (
-      contextSrv.hasPermissionInMetadata(AccessControlAction.DataSourcesPermissionsRead, dataSource) ||
-      shouldEnableFeatureHighlights
-    ) {
-      navModel.children!.push(dsPermissions);
-    }
-  } else if (highlightsEnabled && !isLoadingNav) {
-    navModel.children!.push({
-      ...dsPermissions,
-      url: dsPermissions.url + '/upgrade',
-      tabSuffix: () => ProBadge({ experimentId: permissionsExperimentId }),
-    });
-  }
-
-  if (config.analytics?.enabled || shouldEnableFeatureHighlights) {
-    const analyticsExperimentId = 'feature-highlights-data-source-insights-badge';
-    const analytics: NavModelItem = {
-      active: false,
-      icon: 'info-circle',
-      id: `datasource-insights-${dataSource.uid}`,
-      text: t('datasources.build-nav-model.analytics.text.insights', 'Insights'),
-      url: `datasources/edit/${dataSource.uid}/insights`,
-    };
-
-    if ((highlightTrial() && !isLoadingNav) || shouldEnableFeatureHighlights) {
-      analytics.tabSuffix = () => ProBadge({ experimentId: analyticsExperimentId, eventVariant: 'trial' });
-    }
-
-    if (featureEnabled('analytics') || shouldEnableFeatureHighlights) {
-      if (contextSrv.hasPermission(AccessControlAction.DataSourcesInsightsRead) || shouldEnableFeatureHighlights) {
-        navModel.children!.push(analytics);
-      }
-    } else if (highlightsEnabled && !isLoadingNav) {
-      navModel.children!.push({
-        ...analytics,
-        url: analytics.url + '/upgrade',
-        tabSuffix: () => ProBadge({ experimentId: analyticsExperimentId }),
-      });
-    }
-  }
-
-  const cachingExperimentId = 'feature-highlights-query-caching-badge';
-
-  const caching: NavModelItem = {
-    active: false,
-    icon: 'database',
-    id: `datasource-cache-${dataSource.uid}`,
-    text: t('datasources.build-nav-model.caching.text.cache', 'Cache'),
-    url: `datasources/edit/${dataSource.uid}/cache`,
-    hideFromTabs: !pluginMeta.isBackend || !config.caching.enabled,
-  };
-
-  if ((highlightTrial() && !isLoadingNav) || shouldEnableFeatureHighlights) {
-    caching.tabSuffix = () => ProBadge({ experimentId: cachingExperimentId, eventVariant: 'trial' });
-  }
-
-  if (featureEnabled('caching') || shouldEnableFeatureHighlights) {
-    if (
-      contextSrv.hasPermissionInMetadata(AccessControlAction.DataSourcesCachingRead, dataSource) ||
-      shouldEnableFeatureHighlights
-    ) {
-      navModel.children!.push(caching);
-    }
-  } else if (highlightsEnabled && !isLoadingNav) {
-    navModel.children!.push({
-      ...caching,
-      url: caching.url + '/upgrade',
-      tabSuffix: () => ProBadge({ experimentId: cachingExperimentId }),
-    });
-  }
-
   return navModel;
+}
+
+function hasDashboards(includes: PluginInclude[]): boolean {
+  return (
+    includes.find((include) => {
+      return include.type === 'dashboard';
+    }) !== undefined
+  );
 }
 
 export function getDataSourceNav(main: NavModelItem, pageName: string): NavModel {
@@ -213,12 +125,4 @@ export function getDataSourceLoadingNav(pageName: string): NavModel {
   );
 
   return getDataSourceNav(main, pageName);
-}
-
-function hasDashboards(includes: PluginInclude[]): boolean {
-  return (
-    includes.find((include) => {
-      return include.type === 'dashboard';
-    }) !== undefined
-  );
 }
