ff9050f2f727c5defa6fcd9759f90532682c8e30
Sujeet Kausallya Gholap <sujeet.gholap@oodle.ai>
2026-03-28 06:02:14 +0530

    feat: expose `window.locationService` (#130)
    Ref D-6706
    
    This would allow parent frame to trigger SPA
    navigations inside Grafana iframes.

--- public/app/features/runtime/init.ts
+++ public/app/features/runtime/init.ts
@@ -1,4 +1,5 @@
 import { PanelData, RawTimeRange } from '@grafana/data';
+import { locationService, LocationService } from '@grafana/runtime';
 import { getDashboardSrv } from 'app/features/dashboard/services/DashboardSrv';
 import { getTimeSrv } from 'app/features/dashboard/services/TimeSrv';
 
@@ -6,6 +7,7 @@ import { DashboardModel } from '../dashboard/state/DashboardModel';
 
 declare global {
   interface Window {
+    locationService?: LocationService;
     grafanaRuntime?: {
       getDashboardSaveModel: () => DashboardModel | undefined;
       getDashboardTimeRange: () => { from: number; to: number; raw: RawTimeRange };
@@ -53,5 +55,25 @@ export function initWindowRuntime() {
         return acc;
       }, {});
     },
+  };
+
+  if (window.locationService) {
+    console.warn(
+      'window.locationService already exists. Grafana will not overwrite it.'
+    );
+    return;
+  }
+
+  window.locationService = {
+    partial: (query, replace) => locationService.partial(query, replace),
+    push: (location) => locationService.push(location),
+    replace: (location) => locationService.replace(location),
+    reload: () => locationService.reload(),
+    getLocation: () => locationService.getLocation(),
+    getHistory: () => locationService.getHistory(),
+    getSearch: () => locationService.getSearch(),
+    getSearchObject: () => locationService.getSearchObject(),
+    getLocationObservable: () => locationService.getLocationObservable(),
+    update: (update) => locationService.update(update),
   };
 }
